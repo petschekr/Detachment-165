1c88b5035810f71d3d894b98da85ee6f2d23b237
Ryan Petschek <petschekr@gmail.com>
2019-06-07 22:20:56 -0400

    Support local loading of assets during build

--- template-builder/src/main.rs
+++ template-builder/src/main.rs
@@ -1,10 +1,19 @@
 use std::fs;
 use handlebars::Handlebars;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Config {
+    local: bool,
+}
 
 fn main() {
     let files = [
         "index"
     ];
+    let config = Config {
+        local: false,
+    };
 
     let mut handlebars = Handlebars::new();
     handlebars.register_partial("wrapper", fs::read_to_string("../wrapper.hbs").unwrap()).unwrap();
@@ -12,7 +21,7 @@ fn main() {
     for filename in files.iter() {
         let sourcename = format!("../{}.hbs", filename);
         let contents = fs::read_to_string(sourcename).unwrap();
-        let rendered = handlebars.render_template(&contents, &None::<&str>).unwrap();
+        let rendered = handlebars.render_template(&contents, &config).unwrap();
         fs::write(format!("../rendered/{}.html", filename), rendered).unwrap();
     }
 }
