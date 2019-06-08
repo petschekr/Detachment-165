9a57b70e54ab6e68afb42a092ae526ac8ec350ba
Ryan Petschek <petschekr@gmail.com>
2019-06-07 22:45:41 -0400

    Implement automatic builds when file changes

--- template-builder/src/main.rs
+++ template-builder/src/main.rs
@@ -1,10 +1,36 @@
 use std::fs;
+use std::sync::mpsc::channel;
+use std::time::Duration;
 use handlebars::Handlebars;
 use serde::Serialize;
+use notify::{ Watcher, RecursiveMode, watcher };
 
 #[derive(Serialize)]
 struct Config {
     local: bool,
+}
+
+struct Builder {
+    handlebars: Handlebars,
+    config: Config,
+}
+
+impl Builder {
+    fn new(config: Config) -> Self {
+        let mut handlebars = Handlebars::new();
+        handlebars.register_partial("wrapper", fs::read_to_string("../content/wrapper.hbs").unwrap()).unwrap();
+
+        Self { handlebars, config }
+    }
+
+    fn build(&self, files: &[&'static str]) {
+        for filename in files.iter() {
+            let sourcename = format!("../content/{}.hbs", filename);
+            let contents = fs::read_to_string(sourcename).unwrap();
+            let rendered = self.handlebars.render_template(&contents, &self.config).unwrap();
+            fs::write(format!("../rendered/{}.html", filename), rendered).unwrap();
+        }
+    }
 }
 
 fn main() {
@@ -15,13 +41,19 @@ fn main() {
         local: false,
     };
 
-    let mut handlebars = Handlebars::new();
-    handlebars.register_partial("wrapper", fs::read_to_string("../wrapper.hbs").unwrap()).unwrap();
+    let builder = Builder::new(config);
+    builder.build(&files);
 
-    for filename in files.iter() {
-        let sourcename = format!("../{}.hbs", filename);
-        let contents = fs::read_to_string(sourcename).unwrap();
-        let rendered = handlebars.render_template(&contents, &config).unwrap();
-        fs::write(format!("../rendered/{}.html", filename), rendered).unwrap();
+    let (tx, rx) = channel();
+    let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
+    watcher.watch("../content", RecursiveMode::Recursive).unwrap();
+
+    println!("Watching for changes...");
+
+    loop {
+        let _ = rx.recv().unwrap();
+        print!("Building...");
+        builder.build(&files);
+        println!("done");
     }
 }
